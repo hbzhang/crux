336969ed8c49510f154b288cf3562aa3a89c68c4
Rich Collins <richcollins@gmail.com>
2009-02-19 00:56:13 -0800

    latest

--- src/Browser.js
+++ src/Browser.js
@@ -1,7 +1,37 @@
 Browser = Proto.clone().setSlots(
 {
+	userAgent: function()
+	{
+		if(typeof window != "undefined" && typeof window.navigator != "undefined")
+		{
+			return window.navigator.userAgent;
+		}
+		else
+		{
+			return "";
+		}
+	},
+	
 	isInternetExplorer: function()
 	{
 		return navigator.appName.indexOf("Internet Explorer") > -1;
+	},
+	
+	isGecko: function()
+	{
+		this.userAgent().indexOf("Gecko") != -1;
+	},
+	
+	version: function()
+	{
+		if(this.isGecko())
+		{
+			var index = USER_AGENT.indexOf("Firefox");
+			return (index == -1) ? 2.0 : parseFloat(USER_AGENT.substring(index + "Firefox".length + 1));
+		}
+		else
+		{
+			return null;
+		}
 	}
 })
