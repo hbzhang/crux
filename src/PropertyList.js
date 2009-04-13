b06637f6c8a8236d9e2b20f40c156b5168fd8dd3
Rich Collins <richcollins@gmail.com>
2009-04-12 22:53:18 -0700

    PropertyList methods

--- src/PropertyList.js
+++ src/PropertyList.js
@@ -6,3 +6,18 @@ function PropertyList_isEmpty(propertyList)
 	}
 	return true;
 }
+
+function PropertyList_asArray(propertyList)
+{
+	var a = [];
+	for(var name in propertyList)
+	{
+		a.push(propertyList[name]);
+	}
+	return a;
+}
+
+function PropertyList_join(propertyList, separator)
+{
+	return PropertyList_asArray(propertyList).join(separator);
+}
