ee630c0e19675afa4dcefc2e7b33c95109a5acec
Rich Collins <richcollins@gmail.com>
2009-04-13 19:05:31 -0700

    PropertyList_shallowCopy

--- src/PropertyList.js
+++ src/PropertyList.js
@@ -21,3 +21,13 @@ function PropertyList_join(propertyList, separator)
 {
 	return PropertyList_asArray(propertyList).join(separator);
 }
+
+function PropertyList_shallowCopy(propertyList)
+{
+	var copy = {};
+	for(var name in propertyList)
+	{
+		copy[name] = propertyList[name]
+	}
+	return copy;
+}
